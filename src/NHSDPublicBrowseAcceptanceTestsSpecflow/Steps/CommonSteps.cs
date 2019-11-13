e4ee528f3234609ee21c6d5eafb5c04dd9455b57
Jonathan Alsop <Jonathan.Alsop@bjss.com>
2019-11-13 11:33:34 +0000

    Adding CommonSteps class

---
+++ src/NHSDPublicBrowseAcceptanceTestsSpecflow/Steps/CommonSteps.cs
@@ -0,0 +1,22 @@
+﻿using NHSDPublicBrowseAcceptanceTestsSpecflow.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace NHSDPublicBrowseAcceptanceTestsSpecflow.Steps
+{
+    [Binding]
+    public sealed class CommonSteps
+    {
+        private readonly UITest _test;
+        private readonly ScenarioContext _context;
+
+        public CommonSteps(UITest test,  ScenarioContext context)
+        {
+            _test = test;
+            _context = context;
+        }
+    }
+}
