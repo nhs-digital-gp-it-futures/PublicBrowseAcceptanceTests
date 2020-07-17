a513b7a1d853ef1f89d441137d9316d02406297f
Adrian Soomro <adrian.soomro@gmail.com>
2020-07-17 15:06:04 +0100

    Tidy up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-PBHOSTURL=$(echo "$PBURL" | cut -c9-)
-echo "51.11.46.27   $PBHOSTURL" >> /etc/hosts 
-echo "Resolved address for $PBURL"
 ip -4 route list match 0/0 | awk '{print $3" host.docker.internal"}' >> /etc/hosts
 echo "Resolved address for host.docker.internal"
 
