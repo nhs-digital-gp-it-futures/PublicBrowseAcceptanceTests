6c8f15ac54d216b163acc864e0cf369d111938c9
Adrian Soomro <adrian.soomro@gmail.com>
2020-07-17 09:40:58 +0100

    Add more verbosity to the output

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ echo "Resolved address for $PBURL"
 ip -4 route list match 0/0 | awk '{print $3" host.docker.internal"}' >> /etc/hosts
 echo "Resolved address for host.docker.internal"
 
-dotnet test out/NHSDPublicBrowseAcceptanceTests.Tests.dll
+dotnet test out/NHSDPublicBrowseAcceptanceTests.Tests.dll -v n
