4a776b453006d7093b843814291a5de0d903fd28
Adrian Soomro <adrian.soomro@gmail.com>
2020-07-17 10:05:19 +0100

    Use the right syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ echo "Resolved address for $PBURL"
 ip -4 route list match 0/0 | awk '{print $3" host.docker.internal"}' >> /etc/hosts
 echo "Resolved address for host.docker.internal"
 
-apk add curl && curl -k -i $(PBURL) --fail --connect-timeout 30
+apk add curl && curl -k -i $PBURL --fail --connect-timeout 30
 
 dotnet test out/NHSDPublicBrowseAcceptanceTests.Tests.dll -v n
