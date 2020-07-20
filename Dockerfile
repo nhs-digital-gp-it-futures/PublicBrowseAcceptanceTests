da699b18e1edb3bbe3faa63b1450a95f15b63eb1
Adrian Soomro <adrian.soomro@gmail.com>
2020-07-20 14:39:16 +0100

    Enable test result collection

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM mcr.microsoft.com/dotnet/core/sdk:3.1-alpine3.11
 ENV MSBUILDSINGLELOADCONTEXT=1
 WORKDIR /app
-COPY ./src ./
-COPY ./entrypoint.sh ./
+COPY ./src ./entrypoint.sh ./
+RUN apk add --no-cache bash
 RUN dotnet publish -c Release -o out
-ENTRYPOINT ["/bin/sh", "entrypoint.sh"]
+ENTRYPOINT ["/bin/bash", "entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 ip -4 route list match 0/0 | awk '{print $3" host.docker.internal"}' >> /etc/hosts
 echo "Resolved address for host.docker.internal"
 
-dotnet test out/NHSDPublicBrowseAcceptanceTests.Tests.dll -v n
+if [ -n "${TEST_RESULT_DIR}" ]; then
+  echo "directing tests result to '$TEST_RESULT_DIR'."
+  dotnet test out/NHSDPublicBrowseAcceptanceTests.Tests.dll -v n --logger trx --results-directory "$TEST_RESULT_DIR"
+else
+  dotnet test out/NHSDPublicBrowseAcceptanceTests.Tests.dll -v n 
+fi
