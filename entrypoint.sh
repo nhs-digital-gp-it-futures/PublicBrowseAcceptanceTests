fe0d2341754044e5a10ba4dcc7c8a3b680703ba3
Adrian Soomro <adrian.soomro@gmail.com>
2020-07-20 15:47:07 +0100

    Enable Test filters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+additionalDotnetArgs=""
+
 ip -4 route list match 0/0 | awk '{print $3" host.docker.internal"}' >> /etc/hosts
 echo "Resolved address for host.docker.internal"
 
 if [ -n "${TEST_RESULT_DIR}" ]; then
-  echo "directing tests result to '$TEST_RESULT_DIR'."
-  dotnet test out/NHSDPublicBrowseAcceptanceTests.Tests.dll -v n --logger trx --results-directory "$TEST_RESULT_DIR"
-else
-  dotnet test out/NHSDPublicBrowseAcceptanceTests.Tests.dll -v n 
+  echo "Directing test results to '$TEST_RESULT_DIR'"
+  additionalDotnetArgs+="--logger trx --results-directory $TEST_RESULT_DIR "
 fi
+
+if [ -n "${TEST_FILTER}" ]; then
+  echo "Running only tests annotated with '@$TEST_FILTER'"
+  additionalDotnetArgs+="--filter TestCategory=$TEST_FILTER "
+fi
+
+cmd="dotnet test out/NHSDPublicBrowseAcceptanceTests.Tests.dll -v n $additionalDotnetArgs"
+echo -e "\nRunning '$cmd' \n"
+eval $cmd
