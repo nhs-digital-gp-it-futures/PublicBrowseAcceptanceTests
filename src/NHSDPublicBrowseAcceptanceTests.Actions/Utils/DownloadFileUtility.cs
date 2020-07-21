0a30d917576d055da8a3208c4114d9f71d8da6ea
PhysicsJon <j.r.alsop.18@gmail.com>
2020-07-21 14:37:25 +0100

    Dockerise tests (#52)
    Dockerise AC tests
    
    Co-authored-by: Tony Lawson <tony.lawson@bjss.com>
    Co-authored-by: @PhysicsJon

--- src/NHSDPublicBrowseAcceptanceTests.Actions/Utils/DownloadFileUtility.cs
+++ src/NHSDPublicBrowseAcceptanceTests.Actions/Utils/DownloadFileUtility.cs
@@ -8,31 +8,28 @@ namespace NHSDPublicBrowseAcceptanceTests.Actions.Utils
 {
     public static class DownloadFileUtility
     {
-        public static WebClient DownloadFile(string fileName, string downloadPath, string downloadLink, IDictionary<string,string> headers = null)
+        public static WebClient DownloadFile(string fileName, string downloadPath, string downloadLink, IDictionary<string, string> headers = null)
         {
             ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
             downloadLink = TransformLocalHost(downloadLink);
             Directory.CreateDirectory(downloadPath);
-            using (var client = new WebClient())
+            using var client = new WebClient();
+            if (headers != null)
             {
-                if(headers != null)
+                foreach (var (key, value) in headers)
                 {
-                    foreach (var pair in headers)
-                    {
-                        client.Headers.Add(pair.Key, pair.Value);
-                    }
+                    client.Headers.Add(key, value);
                 }
-
-                client.DownloadFile(downloadLink, Path.Combine(downloadPath, fileName));
-                return client;
             }
+
+            client.DownloadFile(downloadLink, Path.Combine(downloadPath, fileName));
+            return client;
         }
 
         public static string TransformLocalHost(string urlIn)
         {
             return urlIn
-                .Replace("host.docker.internal", "localhost")
-                .Replace("gpitfutures-bc-pb.buyingcatalogue", "localhost");
+                .Replace("gpitfutures-bc-pb.buyingcatalogue", "host.docker.internal");
         }
 
         public static bool CompareTwoFiles(string filePath1, string filePath2)
@@ -41,7 +38,7 @@ namespace NHSDPublicBrowseAcceptanceTests.Actions.Utils
                    File.ReadAllBytes(filePath1).SequenceEqual(File.ReadAllBytes(filePath2));
         }
 
-        public static IDictionary<string,string> GetHeadersFromDriver(IWebDriver driver)
+        public static IDictionary<string, string> GetHeadersFromDriver(IWebDriver driver)
         {
             var useragent = ((IJavaScriptExecutor)driver).ExecuteScript("return navigator.userAgent;");
             var referer = ((IJavaScriptExecutor)driver).ExecuteScript("return navigator.referer;");
