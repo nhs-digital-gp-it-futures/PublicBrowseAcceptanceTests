a55929b2aaa06b51214c7991c294932f2a829804
PhysicsJon <j.r.alsop.18@gmail.com>
2021-01-19 14:04:13 +0000

    PR comments

--- src/NHSDPublicBrowseAcceptanceTests.Actions/Interactions.cs
+++ src/NHSDPublicBrowseAcceptanceTests.Actions/Interactions.cs
@@ -1,8 +1,6 @@
-﻿using NHSDPublicBrowseAcceptanceTests.Objects;
-using NHSDPublicBrowseAcceptanceTests.Objects.Pages;
+﻿using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
-using System;
 
 namespace NHSDPublicBrowseAcceptanceTests.Actions
 {
